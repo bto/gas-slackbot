1ba9753b0d2aa5e9ab68d85cb3edcfc56f0090a6
Masato Bito <masato@bz2.jp>
2018-07-18 19:49:47 +0900

    add some tests

--- src/SlackBotAppTest.js
+++ src/SlackBotAppTest.js
@@ -2,14 +2,30 @@ testRunner.functions.push(function (test) {
   test('new SlackBot()', function (assert) {
     var bot = new SlackBot();
     assert.ok(bot instanceof SlackBot, 'creates SlackBot object');
+  });
 
-    var str = bot.getDefaultMessage();
-    assert.ok(typeof str === 'string', 'if message was a string');
-    assert.notEqual(str.length, 0, 'if message was not an empty string');
+  test('SlackBot default message', function (assert) {
+    var bot = new SlackBot();
 
-    str = bot.getUsername();
-    assert.ok(typeof str === 'string', 'if username was a string');
-    assert.notEqual(str.length, 0, 'if username was not an empty string');
+    var message = bot.getDefaultMessage();
+    assert.ok(typeof message === 'string', 'if message was a string');
+    assert.notEqual(message.length, 0, 'if message was not an empty string');
+
+    var obj = bot.setDefaultMessage('default message');
+    assert.equal(bot, obj, 'returns itself');
+    assert.equal('default message', bot.getDefaultMessage(), 'set default message');
+  });
+
+  test('SlackBot username', function (assert) {
+    var bot = new SlackBot();
+
+    var username = bot.getUsername();
+    assert.ok(typeof username === 'string', 'if username was a string');
+    assert.notEqual(username.length, 0, 'if username was not an empty string');
+
+    var obj = bot.setUsername('username');
+    assert.equal(bot, obj, 'returns itself');
+    assert.equal('username', bot.getUsername(), 'set username');
   });
 });
 
