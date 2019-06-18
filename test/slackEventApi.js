03f63fd49b86b7bd8c7a1c3915798acf892a6374
Masato Bito <masato@bz2.jp>
2019-06-18 22:05:26 +0900

    fix by eslint

--- test/slackEventApi.js
+++ test/slackEventApi.js
@@ -1,23 +1,22 @@
 var assert = require('power-assert');
 var supertest = require('supertest');
-var util = require('util');
 
 var url = 'https://script.google.com/a/uuum.jp/macros/s/AKfycbxqOVBusPqKvQY8i8xnOEOU0BBDjTYOM6VYzch9vw/exec';
 var api = supertest(url);
 
-describe('slack event api', function () {
-  it('', function () {
+describe('slack event api', function testSlackEventApi() {
+  it('url verification', function testUrlVerification() {
     return api.post('')
       .redirects(1)
       .set('Content-Type', 'application/json')
       .send({
-        "token": "9ufNZYQBibsMDlLMMlRuZot4",
-        "type": "url_verification",
-        "challenge": "challenge code"
+        'token': '9ufNZYQBibsMDlLMMlRuZot4',
+        'type': 'url_verification',
+        'challenge': 'challenge code'
       })
-      .then(function (res) {
-        assert(res.statusCode == 200);
-        assert(res.text == 'challenge code');
+      .then(function testUrlVerificationResponse(res) {
+        assert(res.statusCode === 200);
+        assert(res.text === 'challenge code');
       });
   });
 });
