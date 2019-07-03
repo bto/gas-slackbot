b786bbf19eab613e97681dc9ad2699bb20bac325
Masato Bito <masato@bz2.jp>
2019-07-03 20:17:16 +0900

    fix config files

--- config/dev/config.js
+++ config/dev/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  'channelId': 'C070J8TDK',
   'url': 'https://script.google.com/macros/s/AKfycbxyQ6Weob4Sossqnf4lAMBWjhWXAcC3e2YuiID0zfGCUaVhh4Ij/exec',
   'verificationToken': '%VERIFICATION_TOKEN%'
 };
--- config/prod/config.js
+++ config/prod/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  'channelId': 'C070J8TDK',
   'url': 'https://script.google.com/macros/s/AKfycbzS6kP0FU5qOP8ZZOfz1Atc6dY9oiz9v00iP_yNIxjyAPzeaHId/exec',
   'verificationToken': '%VERIFICATION_TOKEN%'
 };
