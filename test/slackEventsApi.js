ec2ecf672ca886ccfad74247402c29c1456bc707
Masato Bito <masato@bz2.jp>
2019-07-03 20:16:09 +0900

    fix js test

--- test/slackEventsApi.js
+++ test/slackEventsApi.js
@@ -10,55 +10,50 @@ function createApi() {
     .set('Content-Type', 'application/json');
 }
 
+function createPostData(eventParams, params) {
+  return Object.assign({
+    'event': Object.assign({
+      'type': 'app_mention',
+      'channel': config.channelId
+    }, eventParams ? eventParams : {}),
+    'token': config.verificationToken,
+    'type': 'event_callback'
+  }, params ? params : {});
+}
+
 describe('slack Events API', function testSlackEventsApi() {
   it('url verification', function testUrlVerification() {
-    return createApi().send({
-      'token': config.verificationToken,
+    return createApi().send(createPostData({}, {
       'type': 'url_verification',
       'challenge': 'challenge code'
-    }).then(function testResponse(res) {
+    })).then(function testResponse(res) {
       assert(res.statusCode === 200);
       assert(res.text === 'challenge code');
     });
   });
 
   it('app_mention event unknown command', function testAppMention() {
-    return createApi().send({
-      'token': config.verificationToken,
-      'type': 'event_callback',
-      'event': {
-        'type': 'app_mention',
-        'text': ''
-      }
-    }).then(function testResponse(res) {
+    return createApi().send(createPostData({
+      'text': '<@Uxxx> foo'
+    })).then(function testResponse(res) {
       assert(res.statusCode === 200);
       assert(res.text === 'そんなコマンドはないよ。');
     });
   });
 
   it('app_mention event help', function testAppMention() {
-    return createApi().send({
-      'token': config.verificationToken,
-      'type': 'event_callback',
-      'event': {
-        'type': 'app_mention',
-        'text': '<@Uxxx> help'
-      }
-    }).then(function testResponse(res) {
+    return createApi().send(createPostData({
+      'text': '<@Uxxx> help'
+    })).then(function testResponse(res) {
       assert(res.statusCode === 200);
       assert(res.text === '吾輩はBotである。ヘルプはまだない。');
     });
   });
 
   it('app_mention event ping', function testAppMention() {
-    return createApi().send({
-      'token': config.verificationToken,
-      'type': 'event_callback',
-      'event': {
-        'type': 'app_mention',
-        'text': '<@Uxxx> ping'
-      }
-    }).then(function testResponse(res) {
+    return createApi().send(createPostData({
+      'text': '<@Uxxx> ping'
+    })).then(function testResponse(res) {
       assert(res.statusCode === 200);
       assert(res.text === 'PONG');
     });
