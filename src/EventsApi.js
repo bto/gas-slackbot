0f7c3cd5e76e2b93d555a8bf6e1a7f873eb01923
Masato Bito <masato@bz2.jp>
2019-07-08 15:22:12 +0900

    add event handlers to EventsApi class

--- src/EventsApi.js
+++ src/EventsApi.js
@@ -1,6 +1,83 @@
+/**
+ * Register a Bot command
+ * @param {String} name: a command name
+ * @param {Object} func: a function object to process a command
+ * @return {null} return nothing
+ */
+function registerBotCommand2(name, func) {
+  EventsApi.prototype.botCommands[name] = func;
+}
+
+/**
+ * Register an event handler
+ * @param {String} eventType: an event type
+ * @param {Function} func: an event handler
+ * @return {Object} return itself
+ */
+function registerEventHandler2(eventType, func) {
+  if (!EventsApi.prototype.eventHandlers[eventType]) {
+    EventsApi.prototype.eventHandlers[eventType] = [];
+  }
+
+  EventsApi.prototype.eventHandlers[eventType].push(func);
+}
+
+
 var EventsApi = function EventsApi(bot) {
   this.bot = bot;
+  bot.eventsApi = this;
   this.params = JSON.parse(bot.event.postData.contents);
+};
+
+EventsApi.prototype.botCommands = {};
+EventsApi.prototype.defaultMessage = 'そんなコマンドはないよ。';
+EventsApi.prototype.eventHandlers = {};
+
+/**
+ * Call event handlers
+ * @return {Object} return output value
+ */
+EventsApi.prototype.callEventHandlers = function callEventHandlers() {
+  var type = this.getEventType();
+  var handlers = this.eventHandlers[type];
+  if (!handlers) {
+    console.error('does not have any event handler for ' + type);
+    return null;
+  }
+
+  console.info('call event handlers for ' + type);
+  var output = null;
+  for (var i = 0; i < handlers.length; i++) {
+    output = handlers[i](this.bot, this.params);
+    console.info('output of handler: ' + output);
+  }
+
+  return output;
+};
+
+/**
+ * Execute Events API request
+ * @return {Object} return output value
+ */
+EventsApi.prototype.execute = function execute() {
+  var token = this.bot.getVerificationToken();
+  if (!this.verifyToken(token)) {
+    var message = 'invalid verification token: ' + token;
+    console.error(message);
+    throw new Error(message);
+  }
+
+  var type = this.getCallbackType();
+  switch (type) {
+  case 'event_callback':
+    return this.callEventHandlers();
+  case 'url_verification':
+    return this.getChallengeCode();
+  default:
+    message = 'not supported events api: ' + type;
+    console.error(message);
+    throw new Error(message);
+  }
 };
 
 /**
@@ -20,6 +97,14 @@ EventsApi.prototype.getChallengeCode = function getChallengeCode() {
 };
 
 /**
+ * Get a default message
+ * @return {String} return a default message
+ */
+EventsApi.prototype.getDefaultMessage = function getDefaultMessage() {
+  return this.defaultMessage;
+};
+
+/**
  * Get an event type
  * @return {String} return an event type
  */
@@ -35,3 +120,37 @@ EventsApi.prototype.getEventType = function getEventType() {
 EventsApi.prototype.verifyToken = function verifyToken(token) {
   return this.params.token === token;
 };
+
+
+registerBotCommand2('help', function commandPing() {
+  console.info('help command was called');
+  return '吾輩はBotである。ヘルプはまだない。';
+});
+
+registerBotCommand2('ping', function commandPing() {
+  console.info('ping command was called');
+  return 'PONG';
+});
+
+registerEventHandler2('app_mention', function eventAppMention(bot, params) {
+  var eventsApi = bot.eventsApi;
+  var command = params.event.text.split(/\s+/)[1];
+  console.info('bot command: ' + command);
+  var message;
+  if (eventsApi.botCommands.hasOwnProperty(command)) {
+    console.info('call command handler for ' + command);
+    message = eventsApi.botCommands[command](bot, params);
+  } else {
+    console.info('does not have any command handler for ' + command);
+    message = eventsApi.getDefaultMessage();
+  }
+  console.info('output of command handler: ' + message);
+
+  var token = bot.getBotAccessToken();
+  var channelId = params.event.channel;
+  var webApi = new WebApi(token);
+  webApi.callChatPostMessage(channelId, message);
+  console.info('chat.postMessage(): message: ' + message + ', channelId: ' + channelId + ', botAccessToken: ' + token);
+
+  return message;
+});
--- src/EventsApiTest.js
+++ src/EventsApiTest.js
@@ -1,14 +1,22 @@
 TestRunner.functions.push(function (test, common) {
-  function createApi(params) {
-    return new EventsApi(new Bot(createEvent(params)));
+  function createApi(eventParams, params) {
+    return new EventsApi(createBot(eventParams, params));
   }
 
-  function createEvent(params) {
+  function createBot(eventParams, params) {
+    var bot = new Bot(createEvent(eventParams, params));
+    bot.setBotAccessToken(common.getProperty('SLACK_BOT_ACCESS_TOKEN'));
+    bot.setVerificationToken(common.getProperty('SLACK_VERIFICATION_TOKEN'));
+    return bot;
+  }
+
+  function createEvent(eventParams, params) {
     var body = Obj.merge({
       challenge: 'challenge code',
-      event: {
+      event: Obj.merge({
+        channel: common.getProperty('SLACK_CHANNEL_ID'),
         type: 'app_mention'
-      },
+      }, eventParams ? eventParams : {}),
       token: common.getProperty('SLACK_VERIFICATION_TOKEN'),
       type: 'event_callback'
     }, params ? params : {});
@@ -20,8 +28,20 @@ TestRunner.functions.push(function (test, common) {
     };
   }
 
+  test('registerBotCommand()', function (assert) {
+    var func = function () {};
+    registerBotCommand2('foo', func);
+    assert.equal(EventsApi.prototype.botCommands.foo, func, 'register a command function');
+  });
+
+  test('registerEventHandler()', function (assert) {
+    var func = function () {};
+    registerEventHandler2('foo', func);
+    assert.equal(EventsApi.prototype.eventHandlers.foo[0], func, 'register a event function');
+  });
+
   test('new EventsApi()', function (assert) {
-    var api = new EventsApi(new Bot(createEvent()));
+    var api = new EventsApi(createBot());
     assert.ok(api instanceof EventsApi, 'creates EventsApi object');
 
     assert.throws(
@@ -32,25 +52,85 @@ TestRunner.functions.push(function (test, common) {
     );
   });
 
+  test('EventsApi.callEventHandlers()', function (assert) {
+    var api = createApi({type: 'foo_event'});
+    var f1Called = 0;
+    var f2Called = 0;
+
+    var output = api.callEventHandlers();
+    assert.equal(output, null, 'return null');
+    assert.equal(f1Called, 0, 'first function was not called');
+    assert.equal(f2Called, 0, 'second function was not called');
+
+    registerEventHandler2(
+      'foo_event',
+      function () {
+        f1Called++;
+      }
+    );
+    output = api.callEventHandlers();
+    assert.equal(output, null, 'return null');
+    assert.equal(f1Called, 1, 'first function was called');
+    assert.equal(f2Called, 0, 'second function was not called');
+
+    registerEventHandler2(
+      'foo_event',
+      function () {
+        f2Called++;
+        return 'f2';
+      }
+    );
+    output = api.callEventHandlers();
+    assert.equal(output, 'f2', 'return a valid string');
+    assert.equal(f1Called, 2, 'first function was called');
+    assert.equal(f2Called, 1, 'second function was not called');
+  });
+
+  test('EventsApi.execute(): app_mention', function (assert) {
+    var api = createApi({
+      type: 'app_mention',
+      text: ''
+    });
+    assert.equal(api.execute(), 'そんなコマンドはないよ。', 'has a valid content');
+  });
+
+  test('EventsApi.execute(): app_mention', function (assert) {
+    var api = createApi({
+      type: 'app_mention',
+      text: '<@Uxxx> help'
+    });
+    assert.equal(api.execute(), '吾輩はBotである。ヘルプはまだない。', 'has a valid content');
+  });
+
+  test('EventsApi.execute(): app_mention', function (assert) {
+    var api = createApi({
+      type: 'app_mention',
+      text: '<@Uxxx> ping'
+    });
+    assert.equal(api.execute(), 'PONG', 'has a valid content');
+  });
+
+  test('EventsApi.execute(): url_verification', function (assert) {
+    var api = createApi({}, {challenge: 'foo', type: 'url_verification'});
+    assert.equal(api.execute(), 'foo', 'has a valid content');
+  });
+
   test('EventsApi.getCallbackType()', function (assert) {
-    var api = createApi({type: 'url_verification'});
+    var api = createApi(null, {type: 'url_verification'});
     assert.equal(api.getCallbackType(), 'url_verification', 'returns url_verification');
 
-    api = createApi({type: 'event_callback'});
+    api = createApi(null, {type: 'event_callback'});
     assert.equal(api.getCallbackType(), 'event_callback', 'returns event_callback');
   });
 
   test('EventsApi.getCallbackType()', function (assert) {
-    var api = createApi({challenge: 'challenge code'});
+    var api = createApi(null, {challenge: 'challenge code'});
     assert.equal(api.getChallengeCode(), 'challenge code', 'returns challenge code');
   });
 
   test('EventsApi.getEventType()', function (assert) {
-    var api = createApi({event: {type: 'app_mention'}});
-    assert.equal(api.getEventType(), 'app_mention', 'returns app_mention');
-
-    api = createApi({type: 'event_callback'});
-    assert.equal(api.getCallbackType(), 'event_callback', 'returns event_callback');
+    var api = createApi({type: 'foo_bar'});
+    assert.equal(api.getEventType(), 'foo_bar', 'returns foo_bar');
   });
 
   test('EventsApi.verifyToken()', function (assert) {
