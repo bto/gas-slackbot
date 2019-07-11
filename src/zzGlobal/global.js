a5710aac600b2b5f1eab6d8f0d21891dbeee530d
Masato Bito <masato@bz2.jp>
2019-07-11 16:06:12 +0900

    add global variables

---
+++ src/zzGlobal/global.js
@@ -0,0 +1,7 @@
+var Controller = SlackBot.Controller;
+
+var registerBotCommand   = SlackBot.registerBotCommand;
+var registerEvent        = SlackBot.registerEvent;
+var registerSlashCommand = SlackBot.registerSlashCommand;
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^.*$" }] */
