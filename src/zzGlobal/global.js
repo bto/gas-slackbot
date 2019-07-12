efb689b1b9f4f308b844b989f80fe4c2ce54a966
Masato Bito <masato@bz2.jp>
2019-07-12 19:41:40 +0900

    add registerOutgoingWebhook to global scope

--- src/zzGlobal/global.js
+++ src/zzGlobal/global.js
@@ -1,7 +1,8 @@
 var Controller = SlackBot.Controller;
 
-var registerBotCommand   = SlackBot.registerBotCommand;
-var registerEvent        = SlackBot.registerEvent;
-var registerSlashCommand = SlackBot.registerSlashCommand;
+var registerBotCommand      = SlackBot.registerBotCommand;
+var registerEvent           = SlackBot.registerEvent;
+var registerSlashCommand    = SlackBot.registerSlashCommand;
+var registerOutgoingWebhook = SlackBot.registerOutgoingWebhook;
 
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^.*$" }] */
