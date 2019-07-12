ce937a6bf469f45150c91f3c4e6d8fcdb23c0380
Masato Bito <masato@bz2.jp>
2019-07-12 19:06:32 +0900

    change regular file

--- dist/web.js
+++ dist/web.js
@@ -1,1 +1,23 @@
-../src/test/web.js
+var SLACK_BOT_ACCESS_TOKEN;
+var SLACK_VERIFICATION_TOKEN;
+
+function doPost(e) {
+  var bot = new SlackBot.Controller(e);
+  var properties = PropertiesService.getScriptProperties();
+
+  if (SLACK_BOT_ACCESS_TOKEN) {
+    bot.setBotAccessToken(SLACK_BOT_ACCESS_TOKEN);
+  } else {
+    bot.setBotAccessToken(properties.getProperty('SLACK_BOT_ACCESS_TOKEN'));
+  }
+
+  if (SLACK_VERIFICATION_TOKEN) {
+    bot.setVerificationToken(SLACK_VERIFICATION_TOKEN);
+  } else {
+    bot.setVerificationToken(properties.getProperty('SLACK_VERIFICATION_TOKEN'));
+  }
+
+  return bot.execute();
+}
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^doPost$" }] */
